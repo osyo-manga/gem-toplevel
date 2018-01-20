fe4160822ad7432c803eb487cd883c468c619984
manga_osyo <manga.osyo@gmail.com>
2018-01-21 00:42:17 +0900

    Update.

--- lib/toplevel.rb
+++ lib/toplevel.rb
@@ -1,10 +1,20 @@
 require "toplevel/version"
 
+
 module Toplevel
 	module_function
 	def local &block
 		Module.new {
-			refine Object, &block
+			refine Object do
+# 				p self
+# 				p self.instance_methods(false)
+# 				p Object.instance_methods(false)
+
+				old_methods = instance_methods false
+				class_eval &block
+				new_methods = instance_methods false
+				(new_methods - old_methods).each &method(:private)
+			end
 		}
 	end
 end
--- spec/toplevel_spec.rb
+++ spec/toplevel_spec.rb
@@ -1,35 +1,55 @@
-using Toplevel.local {
+class Object
 	def hoge
 		:hoge
+	end
+end
+
+using Toplevel.local {
+	def foo
+		:foo
 	end
 }
 
 RSpec.describe Toplevel do
 	describe ".local" do
-		it { expect(hoge).to eq :hoge }
+		it { expect(foo).to eq :foo }
+		it { expect{ self.foo }.to raise_error(NoMethodError) }
 
 		describe "other local" do
 			using Toplevel.local {
 				def hoge
-					:other
+					:other_hoge
+				end
+
+				def foo
+					:other_foo
 				end
 			}
-			it { expect(hoge).to eq :other }
+			it { expect(hoge).to eq :other_hoge }
+			it { expect{ self.hoge }.to raise_error(NoMethodError) }
+
+			it { expect(foo).to eq :other_foo }
+			it { expect{ self.foo }.to raise_error(NoMethodError) }
 		end
+
+		it { expect(hoge).to eq :hoge }
+		it { expect(self.hoge).to eq :hoge }
 
 		describe "class scope" do
 			class X
 				using Toplevel.local {
-					def hoge
-						:x_hoge
+					def bar
+						:x_bar
 					end
 				}
 
-				def foo
-					hoge
+				def call
+					bar
 				end
 			end
-			it { expect(X.new.foo).to eq :x_hoge }
+			it { expect(X.new.call).to eq :x_bar }
+			it { expect{ X.new.bar }.to raise_error(NoMethodError) }
+			it { expect{ X.new.foo }.to raise_error(NoMethodError) }
 		end
 	end
 end
